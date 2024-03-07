e13f0bef745467f27a77d576613a0ab96bdb38e9
RaphaelIsaiah <rafvie12@gmail.com>
2024-03-07 04:27:37 +0100

    rendered the songs on the web page.

--- JavaScript Algorithms and Data Structures/Learn Basic String and Array Methods By Building A Music Player/script.js
+++ JavaScript Algorithms and Data Structures/Learn Basic String and Array Methods By Building A Music Player/script.js
@@ -21,6 +21,27 @@
  * const doubledNumbers = numbers.map((number) => number * 2); // doubledNumbers will be [2, 4, 6]
  * Notice that the map() method takes a function as an argument. This is called a callback function, which is a function that is passed to another function as an argument.
  * In the example above, the callback function is (number) => number * 2, and it's run on each element in the numbers array. The map() method then returns a new array with the results.
+ * Right now the songsHTML is an array. If you tried to display this as is, you would see the songs separated by commas. This is not the desired outcome because you want to display the songs as a list. To fix this, you will need to join the array into a single string by using the join() method.
+ * The join() method is used to concatenate all the elements of an array into a single string. It takes an optional parameter called a separator which is used to separate each element of the array. For example:
+ * const exampleArr = ["This", "is", "a", "sentence"];
+ * const sentence = exampleArr.join(" "); // Separator takes a space character
+ * console.log(sentence); // Output: "This is a sentence"
+ * Chain the join() method to your map() method and pass in an empty string for the separator.
+ * To chain multiple methods together, you can call the join() method on the result of the map() method. For example:
+ * array.map().join();
+ * Optional chaining (?.) helps prevent errors when accessing nested properties that might be null or undefined. For example:
+ * const user = {
+ * name: "Quincy",
+ * address: {
+ * city: "San Francisco",
+ * state: "CA",
+ * country: "USA"
+ * },
+ * };
+ * // Accessing nested properties without optional chaining
+ * const state = user.address.state; // CA
+ * // Accessing a non-existent nested property with optional chaining
+ * const zipCode = user.address?.zipCode; // Returns undefined instead of throwing an error
  */
 
 // Declaring Variables
@@ -116,18 +137,31 @@ let userData = {
 
 // This will display the songs in the User Interface (UI)
 const renderSongs = (array) => {
-  const songsHTML = array.map((song) => {
-    return `
-    <li id = "song-${song.id}" class="playlist-song">
-        <button class="playlist-song-info">
+  const songsHTML = array
+    .map((song) => {
+      return `
+        <li id="song-${song.id}" class="playlist-song">
+          <button class="playlist-song-info">
             <span class="playlist-song-title">${song.title}</span>
             <span class="playlist-song-artist">${song.artist}</span>
             <span class="playlist-song-duration">${song.duration}</span>
-        </button>
-        <button class="playlist-song-delete" aria-label="Delete ${song.title}">
-          <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/><path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
-        </button>
-    </li>
-    `;
-  });
+          </button>
+          <button class="playlist-song-delete" aria-label="Delete ${song.title}">
+            <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="8" cy="8" r="8" fill="#4d4d62"/>
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/>
+            </svg>
+          </button>
+        </li>
+      `;
+    })
+    .join("");
+
+  // This will display songsHTML into the <ul> in the index.html
+  playlistSongs.innerHTML = songsHTML;
 };
+
+const sortSongs = () => {};
+
+// Calling the renderSongs here renders the songs in the playlist on the page.
+renderSongs(userData?.songs);
