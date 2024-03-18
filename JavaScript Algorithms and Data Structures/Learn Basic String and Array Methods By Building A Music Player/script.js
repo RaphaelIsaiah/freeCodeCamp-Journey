a38ebd256604a314256a75c8d2ba4b5809c1da04
RaphaelIsaiah <rafvie12@gmail.com>
2024-03-18 03:58:01 +0100

    Pause functionality initialized and implemented

--- JavaScript Algorithms and Data Structures/Learn Basic String and Array Methods By Building A Music Player/script.js
+++ JavaScript Algorithms and Data Structures/Learn Basic String and Array Methods By Building A Music Player/script.js
@@ -81,6 +81,7 @@
  * Before playing the song, you need to make sure it starts from the beginning. This can be achieved by the use of the currentTime property of the audio object.
  * Next, use the classList property and the add() method to add the "playing" class to the playButton element. This will look for the class "playing" in the CSS file and add it to the playButton element.
  * play() is a method from the web audio API for playing an mp3 file.
+ * pause() is a method of the Web Audio API for pausing music files.
  */
 
 // Declaring Variables
@@ -193,6 +194,15 @@ const playSong = (id) => {
   audio.play();
 };
 
+// Initialization of Pause Functionality.
+const pauseSong = () => {
+  // The current time of the song when it is paused is set to the current time of the audio variable.
+  userData.songCurrentTime = audio.currentTime;
+  playButton.classList.remove("playing");
+
+  audio.pause();
+};
+
 // This will display the songs in the User Interface (UI)
 const renderSongs = (array) => {
   const songsHTML = array
@@ -204,9 +214,7 @@ const renderSongs = (array) => {
             <span class="playlist-song-artist">${song.artist}</span>
             <span class="playlist-song-duration">${song.duration}</span>
           </button>
-          <button class="playlist-song-delete" aria-label="Delete ${
-            song.title
-          }">
+          <button class="playlist-song-delete" aria-label="Delete ${song.title}">
             <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
               <circle cx="8" cy="8" r="8" fill="#4d4d62"/>
               <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/>
@@ -231,6 +239,9 @@ playButton.addEventListener("click", () => {
     playSong(userData?.currentSong.id);
   }
 });
+
+// Implementation of Pause Functionality.
+pauseButton.addEventListener("click", pauseSong);
 
 // Functionality to alphabetically sort songs.
 const sortSongs = () => {
