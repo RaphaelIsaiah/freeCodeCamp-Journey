92b5474cfae5c534db0f9b02b53a641e3599e5b6
RaphaelIsaiah <rafvie12@gmail.com>
2024-04-11 15:54:56 +0100

    HTML element selection done

--- JavaScript Algorithms and Data Structures/Certification and Practice Projects/Learn localStorage by Building a Todo App/script.js
+++ JavaScript Algorithms and Data Structures/Certification and Practice Projects/Learn localStorage by Building a Todo App/script.js
@@ -1,7 +1,20 @@
+// HTML/DOM element selection
+const taskForm = document.getElementById("task-form");
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const openTaskFormBtn = document.getElementById("open-task-form-btn");
+const closeTaskFormBtn = document.getElementById("close-task-form-btn");
+const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
+const cancelBtn = document.getElementById("cancel-btn");
+const discardBtn = document.getElementById("discard-btn");
+const tasksContainer = document.getElementById("tasks-container");
+const titleInput = document.getElementById("title-input");
+const dateInput = document.getElementById("date-input");
+const descriptionInput = document.getElementById("description-input");
 
 
 /**
  * Local storage is a web browser feature that lets web applications store key-value pairs persistently within a user's browser. This allows web apps to save data during one session, then retrieve it in a later page session.
-
-In this TODO application, you'll learn how to handle form inputs, manage local storage, perform CRUD (Create, Read, Update, Delete) operations on tasks, implement event listeners, and toggle UI elements.
+ * In this TODO application, you'll learn how to handle form inputs, manage local storage, perform CRUD (Create, Read, Update, Delete) operations on tasks, implement event listeners, and toggle UI elements.
+ * LocalStorage is a web storage feature of JavaScript that lets you persist data by storing the data as a key:value pair.
+ * Create a taskData constant and set it to an empty array. This array will store all the tasks along with their associated data, including title, due date, and description. This storage will enable you to keep track of tasks, display them on the page, and save them to localStorage.
  */
