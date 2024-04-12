33c9f8c6b5f7fe286fba50c7754a735897edd17c
RaphaelIsaiah <rafvie12@gmail.com>
2024-04-12 02:45:13 +0100

    Store and state functionality of user data initialized. Toggle and showModal functionality implemented.

--- JavaScript Algorithms and Data Structures/Certification and Practice Projects/Learn localStorage by Building a Todo App/script.js
+++ JavaScript Algorithms and Data Structures/Certification and Practice Projects/Learn localStorage by Building a Todo App/script.js
@@ -11,10 +11,28 @@ const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
+// Array Store to store and keep track of user task data
+const taskData = [];
+// This variable is used to track the state when editing and discarding tasks.
+let currentTask = {};
+
+// Task-form Modal Toggle functionality.
+openTaskFormBtn.addEventListener("click", () => {
+  taskForm.classList.toggle("hidden");
+});
+
+// Confirm-close-dialog showModal functionality.
+closeTaskFormBtn.addEventListener("click", () => {
+  confirmCloseDialog.showModal();
+});
 
 /**
  * Local storage is a web browser feature that lets web applications store key-value pairs persistently within a user's browser. This allows web apps to save data during one session, then retrieve it in a later page session.
  * In this TODO application, you'll learn how to handle form inputs, manage local storage, perform CRUD (Create, Read, Update, Delete) operations on tasks, implement event listeners, and toggle UI elements.
  * LocalStorage is a web storage feature of JavaScript that lets you persist data by storing the data as a key:value pair.
  * Create a taskData constant and set it to an empty array. This array will store all the tasks along with their associated data, including title, due date, and description. This storage will enable you to keep track of tasks, display them on the page, and save them to localStorage.
+ * In earlier projects, you learned how to add and remove classes from an element with el.classList.add() and el.classList.remove(). Another method to use with the classList property is the toggle method. The toggle method will add the class if it is not present on the element, and remove the class if it is present on the element.
+ * element.classList.toggle("class-to-toggle");
+ * The HTML dialog element has a showModal() method that can be used to display a modal dialog box on a web page.
+ * dialogElement.showModal();
  */
