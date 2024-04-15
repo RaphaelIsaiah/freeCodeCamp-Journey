8eb584d5733685096b42f5309f247e1ed3e2f0c2
RaphaelIsaiah <rafvie12@gmail.com>
2024-04-15 02:53:32 +0100

    Understanding the localStorage method

--- JavaScript Algorithms and Data Structures/Certification and Practice Projects/Learn localStorage by Building a Todo App/script.js
+++ JavaScript Algorithms and Data Structures/Certification and Practice Projects/Learn localStorage by Building a Todo App/script.js
@@ -156,6 +156,24 @@ taskForm.addEventListener("submit", (e) => {
   addOrUpdateTask();
 });
 
+// Understanding the localStorage method.
+// const myTaskArr = [
+//   { task: "Walk the Dog", date: "22-04-2022" },
+//   { task: "Read some books", date: "02-11-2023" },
+//   { task: "Watch football", date: "10-08-2021" },
+// ];
+
+// localStorage.setItem("data", JSON.stringify(myTaskArr));
+
+// // localStorage.removeItem("data");
+// localStorage.clear();
+
+// const getTaskArr = localStorage.getItem("data");
+// console.log(getTaskArr);
+
+// const getTaskArrObj = JSON.parse(localStorage.getItem("data"));
+// console.log(getTaskArrObj);
+
 /**
  * Local storage is a web browser feature that lets web applications store key-value pairs persistently within a user's browser. This allows web apps to save data during one session, then retrieve it in a later page session.
  * In this TODO application, you'll learn how to handle form inputs, manage local storage, perform CRUD (Create, Read, Update, Delete) operations on tasks, implement event listeners, and toggle UI elements.
@@ -191,4 +209,16 @@ taskForm.addEventListener("submit", (e) => {
  * const removedFruits = fruits.splice(1, 2);
  * console.log(fruits); // [ 'mango', 'banana', 'apple' ]
  * console.log(removedFruits); // [ 'date', 'cherry' ]
+ * localStorage offers methods for saving, retrieving, and deleting items. The items you save can be of any JavaScript data type.
+ * For instance, the setItem() method is used to save an item, and the getItem() method retrieves the item. To delete a specific item, you can utilize the removeItem() method, or if you want to delete all items in the storage, you can use clear().
+ * Here's how you can save an item:
+ * localStorage.setItem("key", value); // value could be string, number, or any other data type
+ * If you check the Application tab of your browser console, you'll notice a series of [object Object]. This is because everything you save in localStorage needs to be in string format.
+ * To resolve the issue, wrap the data you're saving in the JSON.stringify() method. Then, check local storage again to observe the results.
+ * Now that you have the myTaskArr array saved in localStorage correctly, you can retrieve it with getItem() by specifying the key you used to save the item.
+ * The item you retrieve is a string, as you saved it with JSON.stringify(). To view it in its original form before saving, you need to use JSON.parse().
+ * Use getItem() to retrieve the myTaskArr array again. This time, wrap it inside JSON.parse(), assign it to the variable getTaskArrObj and log the getTaskArrObj to the console.
+ * Check the console to see the difference between getTaskArr and getTaskObj.
+ * You can use localStorage.removeItem() to remove a specific item and localStorage.clear() to clear all items in the local storage. Remove the data item from local storage and open the console to observe the result. You should see null.
+ * Using localStorage.clear() won't just delete a single item from local storage but will remove all items. Remove localStorage.removeItem() and use localStorage.clear() instead. You don't need to pass in anything. You should also see null in the console.
  */
