c7ac4e69b2632effd0616c1839c9ec8154067502
RaphaelIsaiah <rafvie12@gmail.com>
2024-05-12 05:08:24 +0100

    Handling the ReferenceError: document is not defined.

--- JavaScript Algorithms and Data Structures/Certification and Practice Projects/Learn Recursion By Building a Decimal to Binary Converter/script.js
+++ JavaScript Algorithms and Data Structures/Certification and Practice Projects/Learn Recursion By Building a Decimal to Binary Converter/script.js
@@ -13,44 +13,47 @@ const c = () => {
 console.log(a());
 
 // HTML/DOM Element selection
-const numberInput = document.getElementById("number-input");
-const convertBtn = document.getElementById("convert-btn");
-const result = document.getElementById("result");
+// I am using the conditional statement here to wrap the entire code so that it does not throw the error "ReferenceError: document is not defined" when trying to run the code here in the console instead of a browser console.
+if (typeof document !== "undefined") {
+  const numberInput = document.getElementById("number-input");
+  const convertBtn = document.getElementById("convert-btn");
+  const result = document.getElementById("result");
 
-// Functionality to handle the conversion from decimal to binary.
-const decimalToBinary = (input) => {
-  let binary = "";
+  // Functionality to handle the conversion from decimal to binary.
+  const decimalToBinary = (input) => {
+    let binary = "";
 
-  if (input === 0) {
-    binary = "0";
-  }
+    if (input === 0) {
+      binary = "0";
+    }
 
-  while (input > 0) {
-    binary = (input % 2) + binary; // This will build the binary string from right to left.
-    input = Math.floor(input / 2);
-  }
+    while (input > 0) {
+      binary = (input % 2) + binary; // This will build the binary string from right to left.
+      input = Math.floor(input / 2);
+    }
 
-  result.innerText = binary;
-};
+    result.innerText = binary;
+  };
 
-// Initialization of Functionality to check user input value.
-const checkUserInput = () => {
-  // If statement to check if userinput is valid and not empty, or is not a number.
-  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
-    alert("Please provide a decimal number");
-    return;
-  }
+  // Initialization of Functionality to check user input value.
+  const checkUserInput = () => {
+    // If statement to check if userinput is valid and not empty, or is not a number.
+    if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+      alert("Please provide a decimal number");
+      return;
+    }
 
-  decimalToBinary(parseInt(numberInput.value));
-  numberInput.value = ""; // Clears the number input after conversion
-};
+    decimalToBinary(parseInt(numberInput.value));
+    numberInput.value = ""; // Clears the number input after conversion
+  };
 
-// Implementation of functionality to check user input value.
-convertBtn.addEventListener("click", checkUserInput);
+  // Implementation of functionality to check user input value.
+  convertBtn.addEventListener("click", checkUserInput);
 
-// Implementatiion of functionality to check user input value with the enter key.
-numberInput.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    checkUserInput();
-  }
-});
+  // Implementatiion of functionality to check user input value with the enter key.
+  numberInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      checkUserInput();
+    }
+  });
+}
