7e4c4eedf4903c9bcf03fef3f47539409ec53d4e
RaphaelIsaiah <rafvie12@gmail.com>
2024-05-18 02:41:23 +0100

    Added functionality to determine the winner of the game.

--- JavaScript Algorithms and Data Structures/Certification and Practice Projects/Review DOM Manipulation by Building a Rock, Paper, Scissors Game/script.js
+++ JavaScript Algorithms and Data Structures/Certification and Practice Projects/Review DOM Manipulation by Building a Rock, Paper, Scissors Game/script.js
@@ -60,12 +60,26 @@ const getRoundResults = (userOption) => {
 const playerScoreSpanElement = document.getElementById("player-score");
 const computerScoreSpanElement = document.getElementById("computer-score");
 const roundResultsMsg = document.getElementById("results-msg");
+const winnerMsgElement = document.getElementById("winner-msg");
+const optionsContainer = document.querySelector(".options-container");
+const resetGameBtn = document.getElementById("reset-game-btn");
 
 // Functionality to update the scores and result message.
 const showResults = (userOption) => {
   roundResultsMsg.innerText = getRoundResults(userOption);
   playerScoreSpanElement.innerText = playerScore;
   computerScoreSpanElement.innerText = computerScore;
+
+  // Checks the scores to determine the winner of the game.
+  if (playerScore === 3) {
+    winnerMsgElement.innerText = "Player has won the game!";
+    resetGameBtn.style.display = "block";
+    optionsContainer.style.display = "none";
+  } else if (computerScore === 3) {
+    winnerMsgElement.innerText = "Computer has won the game!";
+    resetGameBtn.style.display = "block";
+    optionsContainer.style.display = "none";
+  }
 };
 
 showResults("Rock");
