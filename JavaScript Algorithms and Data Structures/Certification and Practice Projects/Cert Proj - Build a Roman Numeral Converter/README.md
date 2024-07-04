64469d3d043c2097737176a162b9421c96d15cc0
RaphaelIsaiah <rafvie12@gmail.com>
2024-07-04 18:36:56 +0100

    Added functionality to check when input value is less than. Added  functionality to respond when the enter key is struck.

--- JavaScript Algorithms and Data Structures/Certification and Practice Projects/Cert Proj - Build a Roman Numeral Converter/README.md
+++ JavaScript Algorithms and Data Structures/Certification and Practice Projects/Cert Proj - Build a Roman Numeral Converter/README.md
@@ -7,7 +7,7 @@ You should have a button element with an id of "convert-btn" -- check
 
 You should have a div, span or p element with an id of output -- check
 
-When you click on the #convert-btn element without entering a value into the #number element, the #output element should contain the text "Please enter a valid number"
+When you click on the #convert-btn element without entering a value into the #number element, the #output element should contain the text "Please enter a valid number" -- check
 
 When the #number element contains the number -1 and the #convert-btn element is clicked, the #output element should contain the text "Please enter a number greater than or equal to 1"
 
--- JavaScript Algorithms and Data Structures/Certification and Practice Projects/Cert Proj - Build a Roman Numeral Converter/script.js
+++ JavaScript Algorithms and Data Structures/Certification and Practice Projects/Cert Proj - Build a Roman Numeral Converter/script.js
@@ -4,14 +4,24 @@ const number = document.getElementById("number");
 const output = document.getElementById("output");
 const resultDiv = document.getElementById("result-div");
 
-
 // Input Error Handling
 const inputError = () => {
-    // if input is empty
-    if (number.value === "") {
-        resultDiv.classList.remove("hidden");
-        output.innerText = "Please enter a valid number";
-    }
+  // if input is empty
+  if (number.value === "") {
+    resultDiv.classList.remove("hidden");
+    output.innerText = "Please enter a valid number";
+  } else if (number.value < 0) {
+    resultDiv.classList.remove("hidden");
+    output.innerText = "Please enter a number greater than or equal to 1";
+  }
 };
 
+// Implementation of convertButton functionality
 convertBtn.addEventListener("click", inputError);
+
+// Implementatiion of functionality to check user input value with the enter key.
+number.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    inputError();
+  }
+});
