4b7a555e42dbb0e4aa463b9d20d2c4c60c0d6523
RaphaelIsaiah <rafvie12@gmail.com>
2024-07-06 16:09:29 +0100

    Code formats, project completed.

--- JavaScript Algorithms and Data Structures/Certification and Practice Projects/Cert Proj - Build a Roman Numeral Converter/script.js
+++ JavaScript Algorithms and Data Structures/Certification and Practice Projects/Cert Proj - Build a Roman Numeral Converter/script.js
@@ -7,47 +7,47 @@ const resultDiv = document.getElementById("result-div");
 // Fuctionality to convert the user's input.
 
 // No 1
-const checkInputForConversion = () => {
-  let inputInt = parseInt(number.value);
+// const checkInputForConversion = () => {
+//   let inputInt = parseInt(number.value);
 
-  const romanNumeralKeys = {
-    M: 1000,
-    CM: 900,
-    D: 500,
-    CD: 400,
-    C: 100,
-    XC: 90,
-    L: 50,
-    XL: 40,
-    X: 10,
-    IX: 9,
-    V: 5,
-    IV: 4,
-    I: 1,
-  };
+//   const romanNumeralKeys = {
+//     M: 1000,
+//     CM: 900,
+//     D: 500,
+//     CD: 400,
+//     C: 100,
+//     XC: 90,
+//     L: 50,
+//     XL: 40,
+//     X: 10,
+//     IX: 9,
+//     V: 5,
+//     IV: 4,
+//     I: 1,
+//   };
 
-  let romanNumeral = "";
+//   let romanNumeral = "";
 
-  if (number.value === "") {
-    output.innerText = "Please enter a valid number";
-  } else if (inputInt <= 0) {
-    output.innerText = "Please enter a number greater than or equal to 1";
-  } else if (inputInt >= 4000) {
-    output.innerText = "Please enter a number less than or equal to 3999";
-  } else {
-    for (const key in romanNumeralKeys) {
-      while (inputInt >= romanNumeralKeys[key]) {
-        romanNumeral += key;
-        inputInt -= romanNumeralKeys[key];
-      }
-    }
+//   if (number.value === "") {
+//     output.innerText = "Please enter a valid number";
+//   } else if (inputInt <= 0) {
+//     output.innerText = "Please enter a number greater than or equal to 1";
+//   } else if (inputInt >= 4000) {
+//     output.innerText = "Please enter a number less than or equal to 3999";
+//   } else {
+//     for (const key in romanNumeralKeys) {
+//       while (inputInt >= romanNumeralKeys[key]) {
+//         romanNumeral += key;
+//         inputInt -= romanNumeralKeys[key];
+//       }
+//     }
 
-    output.innerText = romanNumeral;
-  }
+//     output.innerText = romanNumeral;
+//   }
 
-  resultDiv.classList.remove("hidden");
-  console.log(romanNumeral);
-};
+//   resultDiv.classList.remove("hidden");
+//   console.log(`Roman numeral for ${number.value} is -- ${romanNumeral}`);
+// };
 
 // No 2
 const convertInput = () => {
@@ -91,15 +91,15 @@ const convertInput = () => {
   }
 
   resultDiv.classList.remove("hidden");
-  console.log(romanNumeral);
+  console.log(`Roman numeral for ${number.value} is -- ${romanNumeral}`);
 };
 
 // Implementation of convertButton functionality
-convertBtn.addEventListener("click", checkInputForConversion);
+convertBtn.addEventListener("click", convertInput);
 
 // Implementatiion of functionality to check user input value with the enter key.
 number.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
-    checkInputForConversion();
+    convertInput();
   }
 });
