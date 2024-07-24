f5ee3cc2c5e2383773c4c9a0cdc96c656de48692
RaphaelIsaiah <rafvie12@gmail.com>
2024-07-24 15:58:41 +0100

    Solved the page refresh bug

--- JavaScript Algorithms and Data Structures/Certification and Practice Projects/Learn Advanced Array Methods by Building a Statistics Calculator/script.js
+++ JavaScript Algorithms and Data Structures/Certification and Practice Projects/Learn Advanced Array Methods by Building a Statistics Calculator/script.js
@@ -1,26 +1,31 @@
 // document.addEventListener("DOMContentLoaded"...) Ensures that the code runs when the HTML document is fully parsed and ready for manipulation.
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("myForm");
+// document.addEventListener("DOMContentLoaded", () => {
+// const form = document.getElementById("myForm");
 
-  // getMean Functionality
-  const getMean = (array) =>
-    array.reduce((acc, el) => acc + el, 0) / array.length;
-  // .reduce() condenses the array into a single value; in this case sums up the values in the array.
+// getMean Functionality
+const getMean = (array) =>
+  array.reduce((acc, el) => acc + el, 0) / array.length;
+// .reduce() condenses the array into a single value; in this case sums up the values in the array.
 
-  // Calculate Functionality
-  const calculate = (event) => {
-    event.preventDefault(); // Stops the page from refreshing after submission.
+// Calculate Functionality
+const calculate = (event) => {
+  // event.preventDefault(); // Stops the page from refreshing after submission.
 
-    // Get the user's inputted values.
-    const value = document.querySelector("#numbers").value;
-    // Split the strings stored in the value variable into an array of substrings.
-    const array = value.split(/,\s*/g);
-    // Chaining the map and filter array methods to the array variable.
-    const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+  // Get the user's inputted values.
+  const value = document.querySelector("#numbers").value;
+  // Split the strings stored in the value variable into an array of substrings.
+  const array = value.split(/,\s*/g);
+  // Chaining the map and filter array methods to the array variable.
+  const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
 
-    console.log(numbers);
-  };
+  // --- Mean ---
+  const mean = getMean(numbers);
 
-  // Event Listeners
-  form.addEventListener("submit", calculate);
-});
+  document.querySelector("#mean").textContent = mean;
+};
+
+// Event Listeners
+// form.addEventListener("submit", calculate);
+// });
+
+// Adding return false; in the HTML file makes all these other commented codes redundant.
