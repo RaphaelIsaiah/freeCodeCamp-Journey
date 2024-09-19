2339c7e6eb7e3ab6a447b8fdc7a575068bb49c43
RaphaelIsaiah <rafvie12@gmail.com>
2024-09-19 16:06:14 +0100

    User stories 1 - 6 fulfilled.

---
+++ JavaScript Algorithms and Data Structures/Certification and Practice Projects/Cert Proj - Build a Telephone Number Validator Project/script.js
@@ -0,0 +1,51 @@
+// HTML DOM SELECTION
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+const userInput = document.getElementById("user-input");
+const resultDiv = document.getElementById("results-div");
+
+// Check user input functionality
+const checkUserInput = () => {
+  const inputValue = userInput.value;
+
+  //   To clear previous results
+  //   resultDiv.innerHTML = "";
+
+  if (inputValue === "") {
+    // Empty entries
+    alert("Please provide a phone number");
+    return;
+  } else if (!isNaN(inputValue)) {
+    resultDiv.classList.remove("hidden");
+
+    const pTag = document.createElement("p");
+    pTag.appendChild(document.createTextNode(`${inputValue} is a number`));
+
+    resultDiv.appendChild(pTag);
+  }
+};
+
+// Clear results functionality
+const clearResults = () => {
+  resultDiv.innerHTML = "";
+  resultDiv.classList.add("hidden");
+};
+
+// EVENTS
+checkBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  checkUserInput();
+  userInput.value = "";
+});
+
+userInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    checkUserInput();
+    userInput.value = "";
+  }
+});
+
+clearBtn.addEventListener("click", (e) => {
+  clearResults();
+});
