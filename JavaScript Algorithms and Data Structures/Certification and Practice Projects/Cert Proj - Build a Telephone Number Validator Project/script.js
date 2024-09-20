0b0072daff4be410b86f0f987f40a279ebc8398d
RaphaelIsaiah <rafvie12@gmail.com>
2024-09-20 04:41:21 +0100

    validation of userInput functionality done.

--- JavaScript Algorithms and Data Structures/Certification and Practice Projects/Cert Proj - Build a Telephone Number Validator Project/script.js
+++ JavaScript Algorithms and Data Structures/Certification and Practice Projects/Cert Proj - Build a Telephone Number Validator Project/script.js
@@ -4,22 +4,53 @@ const clearBtn = document.getElementById("clear-btn");
 const userInput = document.getElementById("user-input");
 const resultDiv = document.getElementById("results-div");
 
+// Results store
+let resultsArray = [];
+
 // Check user input functionality
 const checkUserInput = () => {
   const inputValue = userInput.value;
+  const validUsRegex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s.-]?\d{3}[\s.-]?\d{4}$/;
 
   if (inputValue === "") {
     // Empty entries
     alert("Please provide a phone number");
     return;
-  } else if (!isNaN(inputValue)) {
-    resultDiv.classList.remove("hidden");
-
-    const pTag = document.createElement("p");
-    pTag.appendChild(document.createTextNode(`${inputValue} is a number`));
-
-    resultDiv.appendChild(pTag);
   }
+
+  //   Test user input value
+  const resultMsg = `${
+    validUsRegex.test(inputValue) ? "Valid" : "Invalid"
+  } US number: ${inputValue}`;
+  const resultObj = {
+    id: Date.now(),
+    message: resultMsg,
+  };
+
+  //   Push the result to the result store
+  resultsArray.push(resultObj);
+
+  //   Sort the results in the array in descending order
+  resultsArray.sort((a, b) => b.id - a.id);
+
+  // Clear previous results
+  resultDiv.innerHTML = "";
+
+  // Display the sorted results in the UI
+  resultsArray.forEach((result) => {
+    const resultElement = document.createElement("p");
+    resultElement.appendChild(document.createTextNode(result.message));
+    resultDiv.appendChild(resultElement);
+
+    if (result.message.includes("Invalid")) {
+      resultElement.classList.add("text-danger");
+    } else {
+      resultElement.classList.add("text-success");
+    }
+  });
+
+  resultDiv.classList.remove("hidden");
+  console.log(resultsArray);
 };
 
 // Clear results functionality
