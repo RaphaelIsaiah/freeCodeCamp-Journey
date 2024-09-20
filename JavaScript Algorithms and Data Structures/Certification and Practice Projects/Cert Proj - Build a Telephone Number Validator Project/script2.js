e855d27c812f5a42e8b5979749bbcd56d1077d18
RaphaelIsaiah <rafvie12@gmail.com>
2024-09-20 10:39:20 +0100

    Refactoring for simplicity and better performance. Created new file for the previous method.

--- JavaScript Algorithms and Data Structures/Certification and Practice Projects/Cert Proj - Build a Telephone Number Validator Project/script2.js
+++ JavaScript Algorithms and Data Structures/Certification and Practice Projects/Cert Proj - Build a Telephone Number Validator Project/script2.js
@@ -4,36 +4,53 @@ const clearBtn = document.getElementById("clear-btn");
 const userInput = document.getElementById("user-input");
 const resultDiv = document.getElementById("results-div");
 
+// Results store
+let resultsArray = [];
+
 // Check user input functionality
 const checkUserInput = () => {
   const inputValue = userInput.value;
-  const validUsRegex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s.-]?\d{3}[\s.-]?\d{4}$/;
+  const validUsRegex = /^(1\s?)?(\(\d{3}\)\s?|\d{3}[\s.-]?)\d{3}[\s.-]?\d{4}$/;
 
+  // Empty entries
   if (inputValue === "") {
-    // Empty entries
     alert("Please provide a phone number");
     return;
   }
 
+  // Validate user input value
   const resultMsg = `${
     validUsRegex.test(inputValue) ? "Valid" : "Invalid"
   } US number: ${inputValue}`;
 
+  // Store the resultMsg in a new object
+  const resultObj = {
+    id: Date.now(),
+    message: resultMsg,
+  };
+
+  // Push the new object to the resultsArray store
+  resultsArray.push(resultObj);
+
+  // Sort the result objects in the array in descending order
+  resultsArray.sort((a, b) => b.id - a.id);
+
   // Clear previous results
   resultDiv.innerHTML = "";
 
-  // Create a new p element
-  const pTag = document.createElement("p");
+  // Display the sorted results in the UI
+  resultsArray.forEach((result) => {
+    const resultElement = document.createElement("p");
+    resultElement.appendChild(document.createTextNode(result.message));
+    resultDiv.appendChild(resultElement);
 
-  validUsRegex.test(inputValue)
-    ? pTag.classList.add("text-success")
-    : pTag.classList.add("text-danger");
-
-  // Add the result message to the p element
-  pTag.appendChild(document.createTextNode(resultMsg));
-
-  // Append it to the results div
-  resultDiv.appendChild(pTag);
+    // Style the result text
+    if (result.message.includes("Invalid")) {
+      resultElement.classList.add("text-danger");
+    } else {
+      resultElement.classList.add("text-success");
+    }
+  });
 
   resultDiv.classList.remove("hidden");
 };
