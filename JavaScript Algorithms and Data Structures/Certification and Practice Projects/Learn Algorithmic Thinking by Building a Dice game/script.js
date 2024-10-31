504c2eade75d72659cbb943ad0174ef59cd85bb3
RaphaelIsaiah <rafvie12@gmail.com>
2024-10-31 15:24:35 +0100

    Step 1 complete.

---
+++ JavaScript Algorithms and Data Structures/Certification and Practice Projects/Learn Algorithmic Thinking by Building a Dice game/script.js
@@ -0,0 +1,22 @@
+// HTML DOM ELEMENT SELECTION
+const listOfAllDice = document.querySelectorAll(".die");
+const scoreInputs = document.querySelectorAll("#score-options input");
+const scoreSpans = document.querySelectorAll("#score-options span");
+const roundElement = document.getElementById("current-round");
+const rollsElement = document.getElementById("current-round-rolls");
+const totalScoreElement = document.getElementById("total-score");
+const scoreHistory = document.getElementById("score-history");
+const rollDiceBtn = document.getElementById("roll-dice-btn");
+const keepScoreBtn = document.getElementById("keep-score-btn");
+const rulesBtn = document.getElementById("rules-btn");
+const rulesContainer = document.getElementsByClassName("rules-container")[0];
+
+let isModalShowing = false;
+let diceValuesArr = [];
+let score = 0;
+let round = 1;
+let rolls = 0;
+
+rulesBtn.addEventListener("click", () => {
+  rulesContainer.style.display = "block";
+});
