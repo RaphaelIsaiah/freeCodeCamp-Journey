fa5f1e0ee710cb313a45ebadae68ef3f7b78554d
RaphaelIsaiah <rafvie12@gmail.com>
2024-12-24 16:51:55 +0100

    Add fallback avatars and enhance user avatar handling in post display, project complete.

--- JavaScript Algorithms and Data Structures/Certification and Practice Projects/Learn Asynchronous Programming by Building an FCC Forum Leaderboard/script.js
+++ JavaScript Algorithms and Data Structures/Certification and Practice Projects/Learn Asynchronous Programming by Building an FCC Forum Leaderboard/script.js
@@ -75,6 +75,42 @@ const viewCount = (views) => {
   return views;
 };
 
+// Array of Fallback URLs
+const fallbackAvatarUrls = [
+  "https://i.pinimg.com/736x/34/76/82/347682e28f3b2782cd556687a0e8bf4d.jpg",
+  "https://i.pinimg.com/736x/5b/da/fc/5bdafcaccaded668743405405fc055f7.jpg",
+  "https://i.pinimg.com/736x/8c/a5/26/8ca526b2042d2dae10511a80f3d5866a.jpg",
+  "https://i.pinimg.com/736x/52/46/49/524649971a55b2f3a0dae1d537c61098.jpg",
+  "https://i.pinimg.com/736x/dd/48/cc/dd48cc3579abfa3b1e725f9b0b13bc65.jpg",
+  "https://i.pinimg.com/736x/a2/5f/f0/a25ff04aea0d40dae6e863fc146f10aa.jpg",
+  "https://i.pinimg.com/736x/05/74/ae/0574aefbafcfa5da7b8b9d3d306130c8.jpg",
+];
+
+// Function to get Random Fallback Avatar
+const getRandomFallbackAvatar = () => {
+  const randomIndex = Math.floor(Math.random() * fallbackAvatarUrls.length);
+  return fallbackAvatarUrls[randomIndex];
+};
+
+// Function to get the avatars of the users.
+const avatars = (posters, users) => {
+  return posters
+    .map((poster) => {
+      const user = users.find((user) => user.id === poster.user_id);
+      if (user) {
+        const avatar = user.avatar_template.replace(/{size}/, 30);
+        const userAvatarUrl = avatar.startsWith("/user_avatar/")
+          ? avatarUrl.concat(avatar)
+          : avatar;
+        console.log(`User: ${user.name}, Avatar URL: ${userAvatarUrl}`); // Debugging log
+        return `<img src="${userAvatarUrl}" alt="${
+          user.name
+        }" onerror="this.onerror=null; this.src='${getRandomFallbackAvatar()}';"/>`;
+      }
+    })
+    .join("");
+};
+
 // Fetching data from the API.
 const fetchData = async () => {
   try {
@@ -107,9 +143,14 @@ const showLatestPosts = (data) => {
       return `
     <tr>
       <td>
-        <p class="post-title">${title}</p>
+        <a target="_blank" href="${forumTopicUrl}${slug}/${id}" class="post-title">${title}</a>
+        ${forumCategory(category_id)}
       </td>
-      <td></td>
+      <td>
+        <div class="avatar-container">
+          ${avatars(posters, users)}
+        </div>
+      </td>
       <td>${posts_count - 1}</td>
       <td>${viewCount(views)}</td>
       <td>${timeAgo(bumped_at)}</td>
