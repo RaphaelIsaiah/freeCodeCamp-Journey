6d5ee3c63f498a7c32379b80324326036e56e0e9
RaphaelIsaiah <rafvie12@gmail.com>
2025-01-17 14:32:10 +0100

    Add lifecycle method example and implement conditional rendering in OnlyEvens component

--- Front End Development Libraries/React/Test Vite/fcc-teaches-react/src/Components/Examples/Sample.jsx
+++ Front End Development Libraries/React/Test Vite/fcc-teaches-react/src/Components/Examples/Sample.jsx
@@ -254,4 +254,45 @@ class MyComponent extends React.Component {
   // Change code above this line
 }
 
-export default MyComponent;
+class OnlyEvens extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("Should I update?");
+    // Change code below this line
+    return nextProps.value % 2 === 0;
+    // Change code above this line
+  }
+  componentDidUpdate() {
+    console.log("Component re-rendered.");
+  }
+  render() {
+    return <h1>{this.props.value}</h1>;
+  }
+}
+
+class Controller extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+    };
+    this.addValue = this.addValue.bind(this);
+  }
+  addValue() {
+    this.setState((state) => ({
+      value: state.value + 1,
+    }));
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.addValue}>Add</button>
+        <OnlyEvens value={this.state.value} />
+      </div>
+    );
+  }
+}
+
+export default Controller;
