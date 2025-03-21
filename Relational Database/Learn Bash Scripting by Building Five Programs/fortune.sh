0ab842176a1285549c4eadb01659c64fbbe62e7a
RaphaelIsaiah <rafvie12@gmail.com>
2025-03-21 03:16:17 +0100

    Refactor fortune teller script to improve user input handling and enhance response output

--- Relational Database/Learn Bash Scripting by Building Five Programs/fortune.sh
+++ Relational Database/Learn Bash Scripting by Building Five Programs/fortune.sh
@@ -2,14 +2,26 @@
 # Program to tell a persons fortune
 
 echo -e "\n~~ Fortune Teller ~~\n"
+
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 N=$((RANDOM % 6))
-GET_FORTUNE() {
-    echo Ask a yes or no question:
+
+function GET_FORTUNE() {
+    if [[ ! $1 ]]; then
+        echo Ask a yes or no question:
+    else
+        echo Try again. Make sure it ends with a question mark:
+    fi
+
     read QUESTION
 }
+
+# Prints the initial sentence the first time it runs
+GET_FORTUNE
+
 # checks if QUESTION ends with a ? mark.
 until [[ $QUESTION =~ \?$ ]]; do
-    GET_FORTUNE
+    GET_FORTUNE again
 done
-echo ${RESPONSES[$N]}
+
+echo -e "\n${RESPONSES[$N]}"
