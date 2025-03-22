3ebc954d364e03062a0b55d49c573efb3ac4c4c8
RaphaelIsaiah <rafvie12@gmail.com>
2025-03-22 09:13:18 +0100

    Fix insert_data.sh script to correctly handle major and course insertion logic

--- Relational Database/Learn SQL by Building a Student Database - Part 1/insert_data.sh
+++ Relational Database/Learn SQL by Building a Student Database - Part 1/insert_data.sh
@@ -5,34 +5,42 @@
 # PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
 PSQL="psql -X --username=postgres --dbname=students --no-align --tuples-only -c"
 
+echo $($PSQL "TRUNCATE students, majors, courses, majors_courses")
+
 cat courses_test.csv | while IFS="," read MAJOR COURSE; do
     if [[ $MAJOR != "major" ]]; then
-
         # get major_id
         MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+
         # if not found
         if [[ -z $MAJOR_ID ]]; then
-
             # insert major
             INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
-
             if [[ $INSERT_MAJOR_RESULT == "INSERT 0 1" ]]; then
                 echo "Inserted into majors, $MAJOR"
             fi
 
             # get new major_id
+            $MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
 
         fi
 
-    # get course_id
+        # get course_id
+        COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
 
-    # if not found
+        # if not found
+        if [[ -z $COURSE_ID ]]; then
+            # insert course
+            INSERT_COURSE_RESULT=$($PSQL "INSERT INTO courses(course) VALUES('$COURSE')")
+            if [[ $INSERT_COURSE_RESULT == "INSERT 0 1" ]]; then
+                echo "Inserted into courses, $COURSE"
+            fi
 
-    # insert course
+            # get new course_id
+            $COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
+        fi
 
-    # get new course_id
+        # insert into majors_courses
 
-    # insert into majors_courses
     fi
-
 done
